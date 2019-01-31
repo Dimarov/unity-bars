67bd1d7a7f6ac55a20a22f9a2c30885795babe56
Dimarov <awesomedixde@gmail.com>
2019-01-31 18:20:06 +0200

    corplight-mobile

--- src/components/corplight.js
+++ src/components/corplight.js
@@ -8,7 +8,7 @@ import banking from "../images/icons/banking.png"
 import functionality from "../images/icons/functionality.png"
 import design from "../images/icons/design.png"
 
-const StyledBackground = styled.div`
+const Background = styled.div`
   width: 100%;
   height: auto;
   position: relative;
@@ -18,55 +18,87 @@ const StyledBackground = styled.div`
 const Container = styled.div`
   width: 100%;
   height: auto;
+  padding: 0 1rem;
   max-width: 960px;
   margin: 0 auto;
 `
 
-const StyledHeader = styled.div`
+const Header = styled.div`
   width: 100%;
-  height: 100vh;
-  padding-top: 12rem;
-`
+  height: 48rem;
+  padding-top: 24rem;
 
-const StyledTitle = styled.h1`
-  font-family: myriad-pro, sans-serif;
-  font-size: 8rem;
-  font-weight: 600;
-  color: #303030;
-`
+  @media (min-width: 40rem) {
 
-const StyledSpan = styled.span`
-  color: #3EC4E1;
-`
+  }
 
-const StyledSubtitle = styled.p`
-  font-family: myriad-pro, sans-serif;
-  font-size: 2rem;
-  font-weight: 400;
-  margin: 4rem 0;
-  color: rgba(48,48,48,.8);
-`
-
-const StyledButtonContainer = styled.div`
-  width: 100%;
-  display: flex;
-  margin: 1rem 0;
-
-  @media screen and (max-width: 60rem) {
-    margin: .5rem 0;
-    justify-content: space-evenly;
+  @media (min-width: 80rem) {
+    height: 100vh;
+    padding-top: 12rem;
   }
 `
 
-const StyledButton = styled.button`
+const Title = styled.h1`
+  font-family: myriad-pro, sans-serif;
+  font-size: 4rem;
+  font-weight: 600;
+  color: #303030;
+
+  @media (min-width: 40rem) {
+
+  }
+
+  @media (min-width: 80rem) {
+    font-size: 8rem;
+  }
+`
+
+const Span = styled.span`
+  color: #3EC4E1;
+`
+
+const Subtitle = styled.p`
+  font-family: myriad-pro, sans-serif;
+  font-size: 1.5rem;
+  font-weight: 400;
+  margin: 2rem 0;
+  color: rgba(48,48,48,.8);
+
+  @media (min-width: 40rem) {
+
+  }
+
+  @media (min-width: 80rem) {
+    font-size: 2rem;
+    margin: 4rem 0;
+  }
+`
+
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  margin: .5rem 0;
+  justify-content: space-evenly;
+
+  @media (min-width: 40rem) {
+
+  }
+
+  @media (min-width: 80rem) {
+    margin: 1rem 0;
+    justify-content: flex-start;
+  }
+`
+
+const Button = styled.button`
   outline: none;
   border: .125rem solid #303030;
   border-radius: .125rem;
   background: ${props => props.outline ? "none" : "#303030"};
-  padding: .8rem 3rem;
-  margin-right: 1rem;
+  padding: .3rem 1.25rem;
+  margin: 0 .5rem;
+  font-size: .65rem;
   font-family: myriad-pro, sans-serif;
-  font-size: 1.25rem;
   font-weight: 400;
   color: ${props => props.outline ? "#303030" : "#FEFEFE"};
   cursor: pointer;
@@ -82,67 +114,120 @@ const StyledButton = styled.button`
     transform: translateY(.0625rem);
   }
 
-  @media screen and (max-width: 60rem) {
-    padding: .3rem 1.25rem;
-    margin: 0 .5rem;
-    font-size: .65rem;
+  @media (min-width: 40rem) {
+
+  }
+
+  @media (min-width: 80rem) {
+    padding: .8rem 3rem;
+    margin: 0 1rem 0 0;
+    font-size: 1.25rem;
   }
 `
 
-const StyledHeaderImage = styled.img`
+const HeaderImage = styled.img`
   position: absolute;
-  top: 0;
+  top: 0rem;
   right: 0;
   width: 47rem;
 `
 
-const StyledAboutContainer = styled.div`
+const AboutContainer = styled.div`
   height: auto;
   width: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
+
+  @media (min-width: 40rem) {
+
+  }
+
+  @media (min-width: 80rem) {
+    justify-content: space-between;
+    flex-direction: row;
+  }
 `
 
-const StyledImageContainer = styled.div`
-  width: 20rem;
-  height: 40rem;
+const ImageContainer = styled.div`
+  width: 100%;
+  height: 24rem;
+  display: flex;
+  justify-content: center;
+
+  @media (min-width: 40rem) {
+
+  }
+
+  @media (min-width: 80rem) {
+    width: 20rem;
+    height: 40rem;
+  }
 `
 
-const StyledAboutImage = styled.img`
-  width: 20rem;
+const AboutImage = styled.img`
+  height: 20rem;
+  width: auto;
+
+  @media (min-width: 40rem) {
+
+  }
+
+  @media (min-width: 80rem) {
+    width: 20rem;
+    height: auto;
+  }
 `
 
-const StyledInfoContainer = styled.div`
-  width: 36rem;
-  padding: 0 0 0 2rem;
+const InfoContainer = styled.div`
+  width: 100%;
+  padding: 0rem;
   box-sizing: border-box;
   height: 40rem;
+
+  @media (min-width: 40rem) {
+
+  }
+
+  @media (min-width: 80rem) {
+    width: 36rem;
+    height: 40rem;
+    padding: 0 0 0 2rem;
+  }
 `
 
-const StyledInfoTitle = styled.h2`
+const InfoTitle = styled.h2`
   font-family: myriad-pro, sans-serif;
-  font-size: 3rem;
+  font-size: 2rem;
   font-weight: 400;
   color: #fff;
   padding: 1rem 2rem;
   background: #3EC4E1;
   width: 100%;
+
+  @media (min-width: 40rem) {
+
+  }
+
+  @media (min-width: 80rem) {
+    font-size: 3rem;
+  }
 `
 
-const StyledInfoElementContainer = styled.div`
+const InfoElementContainer = styled.div`
   width: 100%;
   height: auto;
   display: flex;
   flex-direction: column;
 `
 
-const StyledInfoElement = styled.div`
+const InfoElement = styled.div`
   width: 100%;
   height: auto;
   display: flex;
   align-content: center;
 `
-const StyledInfoElementIcon = styled.div`
+const InfoElementIcon = styled.div`
   flex: 0 0 6rem;
   height: 6rem;
   display: flex;
@@ -150,88 +235,104 @@ const StyledInfoElementIcon = styled.div`
   align-items: center;
 `
 
-const StyledInfoElementIconImage = styled.img`
+const InfoElementIconImage = styled.img`
 
 `
 
-const StyledInfoElementDescription = styled.div`
+const InfoElementDescription = styled.div`
   height: auto;
   margin-left: 1rem;
 `
 
-const StyledInfoElementDescriptionTitle = styled.h3`
+const InfoElementDescriptionTitle = styled.h3`
   font-family: myriad-pro, sans-serif;
-  font-size: 1.5rem;
+  font-size: 1rem;
   font-weight: 600;
   color: #29333E;
   margin: .5rem 0;
+
+  @media (min-width: 40rem) {
+
+  }
+
+  @media (min-width: 80rem) {
+    font-size: 1.5rem;
+  }
 `
 
-const StyledInfoElementDescriptionText = styled.p`
+const InfoElementDescriptionText = styled.p`
   font-family: myriad-pro, sans-serif;
-  font-size: 1.5rem;
+  font-size: 1rem;
   font-weight: 400;
   line-height: 2rem;
   color: rgba(41,51,62,.8);
+
+  @media (min-width: 40rem) {
+
+  }
+
+  @media (min-width: 80rem) {
+    font-size: 1.5rem;
+  }
 `
-const StyledFunctionalityTitle = styled.h3`
+const FunctionalityTitle = styled.h3`
 
 `
 
 const Corplight = (props) => (
-  <StyledBackground>
-    <StyledHeader>
+  <Background>
+    <Header>
       <Container>
-        <StyledTitle>CorpLight <StyledSpan>Ощадбанк</StyledSpan></StyledTitle>
-        <StyledSubtitle>Інтернет банкінг для МСБ та приватних клієнтів</StyledSubtitle>
-        <StyledButtonContainer>
-          <StyledButton>App Store</StyledButton>
-          <StyledButton>Google Play</StyledButton>
-          <StyledButton outline >Web Demo</StyledButton>
-        </StyledButtonContainer>
-        <StyledHeaderImage src={header} alt="header-mockup" />
+        <HeaderImage src={header} alt="header-mockup" />
+        <Title>CorpLight <Span>Ощадбанк</Span></Title>
+        <Subtitle>Інтернет банкінг для МСБ та приватних клієнтів</Subtitle>
+        <ButtonContainer>
+          <Button>App Store</Button>
+          <Button>Google Play</Button>
+          <Button outline >Web Demo</Button>
+        </ButtonContainer>
       </Container>
-    </StyledHeader>
+    </Header>
     <Container>
-      <StyledAboutContainer>
-        <StyledImageContainer>
-          <StyledAboutImage src={about} alt="about-mockup" />
-        </StyledImageContainer>
-        <StyledInfoContainer>
-          <StyledInfoTitle>Можливості CorpLight</StyledInfoTitle>
-          <StyledInfoElementContainer>
-            <StyledInfoElement>
-              <StyledInfoElementIcon>
-                <StyledInfoElementIconImage src={banking} alt="banking-icon" />
-              </StyledInfoElementIcon>
-              <StyledInfoElementDescription>
-                <StyledInfoElementDescriptionTitle>Онлайн банкінг</StyledInfoElementDescriptionTitle>
-                <StyledInfoElementDescriptionText>Доступ до банківських продуктів та послуг будь-де та будь-коли.</StyledInfoElementDescriptionText>
-              </StyledInfoElementDescription>
-            </StyledInfoElement>
-            <StyledInfoElement>
-              <StyledInfoElementIcon>
-                <StyledInfoElementIconImage src={functionality} alt="functionality-icon" />
-              </StyledInfoElementIcon>
-              <StyledInfoElementDescription>
-                <StyledInfoElementDescriptionTitle>Функціональність</StyledInfoElementDescriptionTitle>
-                <StyledInfoElementDescriptionText>Функціональність, що відповідає сучасним вимогам клієнтів.</StyledInfoElementDescriptionText>
-              </StyledInfoElementDescription>
-            </StyledInfoElement>
-            <StyledInfoElement>
-              <StyledInfoElementIcon>
-                <StyledInfoElementIconImage src={design} alt="design-icon" />
-              </StyledInfoElementIcon>
-              <StyledInfoElementDescription>
-                <StyledInfoElementDescriptionTitle>Дизайн</StyledInfoElementDescriptionTitle>
-                <StyledInfoElementDescriptionText>Дизайн системи побудований на базі інтуїтивних вподобань користувача. Згідно стандартів Material design Android та IOS Human Interface Guidelines.</StyledInfoElementDescriptionText>
-              </StyledInfoElementDescription>
-            </StyledInfoElement>
-          </StyledInfoElementContainer>
-        </StyledInfoContainer>
-      </StyledAboutContainer>
+      <AboutContainer>
+        <ImageContainer>
+          <AboutImage src={about} alt="about-mockup" />
+        </ImageContainer>
+        <InfoContainer>
+          <InfoTitle>Можливості CorpLight</InfoTitle>
+          <InfoElementContainer>
+            <InfoElement>
+              <InfoElementIcon>
+                <InfoElementIconImage src={banking} alt="banking-icon" />
+              </InfoElementIcon>
+              <InfoElementDescription>
+                <InfoElementDescriptionTitle>Онлайн банкінг</InfoElementDescriptionTitle>
+                <InfoElementDescriptionText>Доступ до банківських продуктів та послуг будь-де та будь-коли.</InfoElementDescriptionText>
+              </InfoElementDescription>
+            </InfoElement>
+            <InfoElement>
+              <InfoElementIcon>
+                <InfoElementIconImage src={functionality} alt="functionality-icon" />
+              </InfoElementIcon>
+              <InfoElementDescription>
+                <InfoElementDescriptionTitle>Функціональність</InfoElementDescriptionTitle>
+                <InfoElementDescriptionText>Функціональність, що відповідає сучасним вимогам клієнтів.</InfoElementDescriptionText>
+              </InfoElementDescription>
+            </InfoElement>
+            <InfoElement>
+              <InfoElementIcon>
+                <InfoElementIconImage src={design} alt="design-icon" />
+              </InfoElementIcon>
+              <InfoElementDescription>
+                <InfoElementDescriptionTitle>Дизайн</InfoElementDescriptionTitle>
+                <InfoElementDescriptionText>Дизайн системи побудований на базі інтуїтивних вподобань користувача. Згідно стандартів Material design Android та IOS Human Interface Guidelines.</InfoElementDescriptionText>
+              </InfoElementDescription>
+            </InfoElement>
+          </InfoElementContainer>
+        </InfoContainer>
+      </AboutContainer>
     </Container>
-  </StyledBackground>
+  </Background>
 )
 
 export default Corplight
--- src/components/projects.js
+++ src/components/projects.js
@@ -8,7 +8,7 @@ import prozorro from "../images/projects/prozorro.png"
 import edata from "../images/projects/edata.png"
 import eauction from "../images/projects/eauction.png"
 
-const StyledBackground = styled.div`
+const Background = styled.div`
   width: 100%;
   height: auto;
   position: relative;
@@ -21,7 +21,7 @@ const Container = styled.div`
   margin: 0 auto;
 `
 
-const StyledHeader = styled.div`
+const Header = styled.div`
   width: 100%;
   height: 32rem;
   background-image: url(${bg});
@@ -30,21 +30,21 @@ const StyledHeader = styled.div`
   padding-top: 8rem;
 `
 
-const StyledTitle = styled.h2`
+const Title = styled.h1`
   font-family: myriad-pro, sans-serif;
   font-size: 2.5rem;
   font-weight: 400;
   color: #fff;
 `
 
-const StyledSubtitle = styled.p`
+const Subtitle = styled.p`
   font-family: myriad-pro, sans-serif;
   font-size: 1.25rem;
   font-weight: 200;
   color: #fff;
 `
 
-const StyledGrid = styled.div`
+const Grid = styled.div`
   height: auto;
   margin: 0 auto;
   max-width: 1440px;
@@ -58,7 +58,7 @@ const StyledGrid = styled.div`
   }
 `
 
-const StyledProject = styled.div`
+const Project = styled.div`
   width: 100%;
   height: 20rem;
   border-radius: .125rem;
@@ -72,7 +72,7 @@ const StyledProject = styled.div`
   }
 `
 
-const StyledProjectPhoto = styled.div`
+const ProjectPhoto = styled.div`
   height: 16rem;
   background-image: url(${props => props.image});
   background-position: center;
@@ -81,13 +81,13 @@ const StyledProjectPhoto = styled.div`
   margin: 1rem 0;
 `
 
- const StyledProjectInfo = styled.div`
+ const ProjectInfo = styled.div`
   flex: 0 0 80%;
   display: flex;
   flex-direction: column;
  `
 
-const StyledProjectTitle = styled.a`
+const ProjectTitle = styled.h2`
   font-family: myriad-pro, sans-serif;
   font-size: 2rem;
   font-weight: 300;
@@ -96,7 +96,7 @@ const StyledProjectTitle = styled.a`
   cursor: pointer;
 `
 
-const StyledProjectSubtitle = styled.h3`
+const ProjectSubtitle = styled.h3`
   font-family: myriad-pro, sans-serif;
   font-size: 1.5rem;
   font-weight: 300;
@@ -104,7 +104,7 @@ const StyledProjectSubtitle = styled.h3`
   margin: .5rem 2rem 1rem;
 `
 
-const StyledProjectText = styled.p`
+const ProjectText = styled.p`
   font-family: myriad-pro, sans-serif;
   font-size: 1rem;
   font-weight: 300;
@@ -113,67 +113,67 @@ const StyledProjectText = styled.p`
 `
 
 const Projects = (props) => (
-  <StyledBackground>
-    <StyledHeader>
+  <Background>
+    <Header>
       <Container>
-        <StyledTitle>Проекти</StyledTitle>
-        <StyledSubtitle>Unity-Bars приймає участь у новітніх проектах України</StyledSubtitle>
+        <Title>Проекти</Title>
+        <Subtitle>Unity-Bars приймає участь у новітніх проектах України</Subtitle>
       </Container>
-    </StyledHeader>
-    <StyledGrid>
-      <StyledProject>
-        <StyledProjectPhoto image={bankid} />
-        <StyledProjectInfo>
-          <StyledProjectTitle>BANKID</StyledProjectTitle>
-          <StyledProjectSubtitle>
+    </Header>
+    <Grid>
+      <Project>
+        <ProjectPhoto image={bankid} />
+        <ProjectInfo>
+          <ProjectTitle>BANKID</ProjectTitle>
+          <ProjectSubtitle>
             Спосіб електронної ідентифікації громадян через українські банки для надання адміністративних послуг через Інтернет
-          </StyledProjectSubtitle>
-          <StyledProjectText>
+          </ProjectSubtitle>
+          <ProjectText>
             Надійна та зручна верифікація користувача через його власний інтернет-банкінг на порталах адміністративних послуг. Станом на сьогодні, на полігоні Ощадбанку, розгорнуто робочий екземпляр системи BankID з повним циклом проходження даних (враховуючи отримання даних по клієнту з банківської бази даних по номеру банківської картки ). Наразі очікується підключення системи до провайдера SMS розсилок (дотепер підключено у тестовому режимі), після чого полігон буде запущено у дослідну експлуатацію на порталі Львівської міської ради Особистий кабінет львів’янина. У інтерфейсі сайту буде доступна кнопка «BankID Ощадбанк», за допомогою якої користувачі матимуть змогу авторизуватися у системі та оформити довідку та звернення з доступного переліку інформації. Також у відкритий доступ на ресурсі GitHub, викладено актуальні схеми та документація по роботі системи BankID з можливістю коментування та редагування.
-          </StyledProjectText>
-        </StyledProjectInfo>
-      </StyledProject>
+          </ProjectText>
+        </ProjectInfo>
+      </Project>
       <hr />
-      <StyledProject>
-        <StyledProjectPhoto image={prozorro} />
-        <StyledProjectInfo>
-          <StyledProjectTitle>Prozorro</StyledProjectTitle>
-          <StyledProjectSubtitle>
+      <Project>
+        <ProjectPhoto image={prozorro} />
+        <ProjectInfo>
+          <ProjectTitle>Prozorro</ProjectTitle>
+          <ProjectSubtitle>
             Системна реформа тендерного процесу в електронних публічних і державних закупівлях
-          </StyledProjectSubtitle>
-          <StyledProjectText>
+          </ProjectSubtitle>
+          <ProjectText>
             Реформу у сфері публічних закупівель можна вважати прикладом успішних змін в країні. Основана мета реформи – суттєве зниження корупції під час закупівель за публічні кошти, досягнення максимальної ефективності та економії. Успішна реалізація проекту має повернути довіру бізнесу до держави та стимулювати його до участі в держаних тендерах. Головний принцип системи ProZorro – «всі бачать все». Тепер бажаючий може дізнатися хто, що і як закуповує за кошти платників податків. Пересічний громадянин можете відслідкувати, що саме закуповує районна поліклініка чи школа, до якої ходять його діти.
-          </StyledProjectText>
-        </StyledProjectInfo>
-      </StyledProject>
+          </ProjectText>
+        </ProjectInfo>
+      </Project>
       <hr />
-      <StyledProject>
-        <StyledProjectPhoto image={edata} />
-        <StyledProjectInfo>
-          <StyledProjectTitle>E-DATA</StyledProjectTitle>
-          <StyledProjectSubtitle>
+      <Project>
+        <ProjectPhoto image={edata} />
+        <ProjectInfo>
+          <ProjectTitle>E-DATA</ProjectTitle>
+          <ProjectSubtitle>
             Єдиний веб-портал використання публічних коштів є офіційним державним інформаційним ресурсом у мережі Інтернет, на якому оприлюднюється інформація згідно з Законом «Про відкритість використання публічних коштів»
-          </StyledProjectSubtitle>
-          <StyledProjectText>
+          </ProjectSubtitle>
+          <ProjectText>
             Доступ до інформації, оприлюдненої на єдиному веб-порталі використання публічних коштів, є вільним та безоплатним, що надає можливість задоволення публічного інтересу щодо процесів контролю, утворення, розподілу та використання публічних коштів розпорядниками та одержувачами коштів Державного бюджету України, бюджету Автономної Республіки Крим і місцевих бюджетів, органами Пенсійного фонду, підприємствами, а також фондами загальнообов’язкового державного соціального страхування.
-          </StyledProjectText>
-        </StyledProjectInfo>
-      </StyledProject>
+          </ProjectText>
+        </ProjectInfo>
+      </Project>
       <hr />
-      <StyledProject>
-        <StyledProjectPhoto image={eauction} />
-        <StyledProjectInfo>
-          <StyledProjectTitle>EAUCTION</StyledProjectTitle>
-          <StyledProjectSubtitle>
+      <Project>
+        <ProjectPhoto image={eauction} />
+        <ProjectInfo>
+          <ProjectTitle>EAUCTION</ProjectTitle>
+          <ProjectSubtitle>
             Автоматизована система проведення електронних аукціонів з оренди та продажу державного майна
-          </StyledProjectSubtitle>
-          <StyledProjectText>
+          </ProjectSubtitle>
+          <ProjectText>
             Після Революції Гідності в українського суспільства з’явились великі сподівання на докорінне перетворення країни. Нові глобальні технологічні зміни та перспективи євроінтеграції відкривають перед українцями нові можливості, якими ми зобов’язані скористатись. Паралельно Україна воює на двох фронтах: проти військової агресії з боку РФ і проти корупцієї, що виснажує країну. Однією з головних сфер, де процвітає корупція, є продаж та оренда держмайна, а також інших прав через «паперові» аукціони. Це негативно впливає на розвиток бізнесу і в цілому на економіку, а держава втрачає величезні гроші. Міжнародний досвід показує, що перехід від "паперових", закритих процедур до відкритих онлайн-систем підвищує ефективність держуправління і поповнює держбюджет на 15-20%, а використання розподілених систем (Blockchain) мінімізує маніпуляції чиновників, що створює позитивний фінансовий і моральний ефект. Іплементація «е-Auction 3.0» створить прозорі умови для проведення державних аукціонів на всіх рівнях та унеможливить існування корупції в цій сфері. UNITY-BARS приєднується до мети ефективного та швидкого реформування, імплементації та розвитку відповідних механізмів!
-          </StyledProjectText>
-        </StyledProjectInfo>
-      </StyledProject>
-    </StyledGrid>
-  </StyledBackground>
+          </ProjectText>
+        </ProjectInfo>
+      </Project>
+    </Grid>
+  </Background>
 )
 
 export default Projects
