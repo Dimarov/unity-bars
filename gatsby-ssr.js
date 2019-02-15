df57188a3c8200d0e9d83ed5421ae0a9650fc484
Dimarov <awesomedixde@gmail.com>
2019-02-15 17:30:36 +0200

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,10 +4,8 @@ import i18n from "./src/i18n"
 
 import createStore from "./src/state/createStore"
 
-const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => (
+export default replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
   i18n.loadNamespaces(["common"], () => {
     replaceBodyHTMLString(bodyComponent)
   })
-)
-
-export default replaceRenderer
+}
