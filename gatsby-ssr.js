0be4f513f361fd9b7bab98eed1f15f35c6870029
Dimarov <awesomedixde@gmail.com>
2019-02-15 17:49:27 +0200

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,7 @@
 import React from "react"
 import { renderToString } from "react-dom/server"
-import i18n from "./src/i18n"
 
-import createStore from "./src/state/createStore"
-
-export default function replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
+export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
   i18n.loadNamespaces(["common"], () => {
     replaceBodyHTMLString(bodyComponent)
   })
