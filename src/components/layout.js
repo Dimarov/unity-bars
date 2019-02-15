941d2ba72427f533771ef6f2165450a0f66cd8fe
Dimarov <awesomedixde@gmail.com>
2019-02-15 10:34:38 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,7 +29,7 @@ const Layout = ({ children }) => (
           ]}>
           <html lang="uk" />
         </Helmet>
-
+        <script src="//instant.page/1.1.0" type="module" integrity="sha384-EwBObn5QAxP8f09iemwAJljc+sU+eUXeL9vSBw1eNmVarwhKk2F9vBEpaN9rsrtp"></script>
         <div>
 
 
