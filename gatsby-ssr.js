272fac486aa14f337a3e0ae51f30c3f858f15b11
Dimarov <awesomedixde@gmail.com>
2019-02-15 17:07:09 +0200

    idontknowwhtahappening

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,13 +1,13 @@
-import React from "react"
-import { renderToString } from "react-dom/server"
-import i18n from "./src/i18n"
-
-import createStore from "./src/state/createStore"
-
-const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
-  i18n.loadNamespaces(["common"], () => {
-    replaceBodyHTMLString(bodyComponent)
-  })
-}
-
-export default replaceRenderer
+// import React from "react"
+// import { renderToString } from "react-dom/server"
+// import i18n from "./src/i18n"
+//
+// import createStore from "./src/state/createStore"
+//
+// const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
+//   i18n.loadNamespaces(["common"], () => {
+//     replaceBodyHTMLString(bodyComponent)
+//   })
+// }
+//
+// export default replaceRenderer
