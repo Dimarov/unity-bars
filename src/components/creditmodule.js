fb24ef957497c5b91ac927f25cf918cb75ef1b4f
Dimarov <awesomedixde@gmail.com>
2019-02-20 10:58:39 +0200

    creditmodule translate

--- src/components/creditmodule.js
+++ src/components/creditmodule.js
@@ -4,6 +4,8 @@ import styled from 'styled-components'
 import { withPrefix } from 'gatsby'
 import office from '../images/fix/office.jpg'
 import bg from '../images/fix/products.jpg'
+import DownloadForm from './download-form'
+import { translate } from 'react-i18next'
 
 
 const StyledBackground = styled.div`
@@ -64,7 +66,7 @@ const StyledHeaderTitle = styled.div`
   font-size: 2.5rem;
   color: #000;
   line-height: 2.5rem;
-  font-weight: 200;
+  font-weight: 400;
   margin: 5rem 0 3rem 0;
 
   @media screen and (max-width: 60rem) {
@@ -79,6 +81,7 @@ const StyledHeaderTitleParagraph = styled.div`
   color: #000;
   font-weight: 400;
   margin: 0 0 1.3rem 0;
+  opacity: .8;
 
   @media screen and (max-width: 60rem) {
     margin: 1.3rem;
@@ -88,119 +91,83 @@ const StyledTestMargin = styled.div`
   margin-bottom: 7rem;
 `
 
-const Creditmodule = (props) => (
-  <StyledBackground>
-    <StyledHeader>
-      <Container>
-        <StyledTitle>Creditmodule</StyledTitle>
-        <StyledSubtitle>Система обслуговування кредитної діяльності та автоматизації кредитної установи з повним циклом
-          обслуговування клієнтів
-        </StyledSubtitle>
-        <StyledDownloadLink href={withPrefix('/documents/CREDITMODULE.pdf')} download="CREDITMODULE.pdf">Завантажити презентацію</StyledDownloadLink>
-      </Container>
-    </StyledHeader>
+class Creditmodule extends React.Component {
 
-    <Container>
-      <StyledHeaderTitle>Функціонал Модуля</StyledHeaderTitle>
-      <StyledHeaderTitleParagraph>
-        &#8226; Заклад кредитного договору — реєстрація позичальника, параметрів кредитної угоди і забезпечення,
-        побудова ГПК,
-        розрахунок ефективної ставки, формування друкованих форм, авторизація за правилом «двох рук»
-      </StyledHeaderTitleParagraph>
+  render() {
+    const { t } = this.props
 
-      <StyledHeaderTitleParagraph>&#8226; Видача коштів — переказ на картковий рахунок, по довільним платіжними
-        реквізитами або
-        видача готівки, викликані
-        здійснених операції
-      </StyledHeaderTitleParagraph>
+    return (
+      <StyledBackground>
+        <StyledHeader>
+          <Container>
+            <StyledTitle>Creditmodule</StyledTitle>
+            <StyledSubtitle>
+              {t("Description")}
+            </StyledSubtitle>
+          </Container>
+        </StyledHeader>
 
-      <StyledHeaderTitleParagraph>
-        &#8226; Обслуговування кредитного договору — нарахування відсотків, пені, винесення на прострочення,
-        автоматичний розбір
-        погашень кредиту і т.д.
-      </StyledHeaderTitleParagraph>
+        <Container>
+          <StyledHeaderTitle>{t("Functionality.Title")}</StyledHeaderTitle>
+          <StyledHeaderTitleParagraph>
+            {t("Functionality.Line1")}
+          </StyledHeaderTitleParagraph>
+          <StyledHeaderTitleParagraph>
+            {t("Functionality.Line2")}
+          </StyledHeaderTitleParagraph>
+          <StyledHeaderTitleParagraph>
+            {t("Functionality.Line3")}
+          </StyledHeaderTitleParagraph>
+          <StyledHeaderTitleParagraph>
+            {t("Functionality.Line4")}
+          </StyledHeaderTitleParagraph>
+          <StyledHeaderTitleParagraph>
+            {t("Functionality.Line5")}
+          </StyledHeaderTitleParagraph>
+          <DownloadForm />
 
-      <StyledHeaderTitleParagraph>
-        &#8226; Погашення кредиту — використання рахунку погашення, планове і дострокове погашення, погашення через БПК,
-        перерахування по СЕП, використання терміналів самообслуговування (Ibox, Бнк24 і т.д.)
-      </StyledHeaderTitleParagraph>
-
-      <StyledHeaderTitleParagraph>
-        &#8226; Реструктуризація — підтримка багатьох видів реструктуризації (пролонгація, зміна% ставки, суми кредиту і
-        т.д.)
-      </StyledHeaderTitleParagraph>
-
-      <StyledHeaderTitleParagraph>
-        &#8226; Погашення кредиту — використання рахунку погашення, планове і дострокове погашення, погашення через БПК,
-        перерахування по СЕП, використання терміналів самообслуговування (Ibox, Бнк24 і т.д.)
-      </StyledHeaderTitleParagraph>
-
-      <StyledHeaderTitleParagraph>
-        &#8226; Реструктуризація — підтримка багатьох видів реструктуризації (пролонгація, зміна% ставки, суми кредиту і
-        т.д.)
-      </StyledHeaderTitleParagraph>
-
-
-      <StyledHeaderTitle>Технологічні Особливості</StyledHeaderTitle>
-      <StyledHeaderTitleParagraph>
-        &#8226; Відображення всіх транзакцій в режимі он-лайн
-      </StyledHeaderTitleParagraph>
-
-      <StyledHeaderTitleParagraph>
-        &#8226; Рольовий підхід — в модулі передбачено набір ролей для виконання різних дій (кредитний інспектор,
-        бухгалтер,
-        аналітик і т.д.), розподіл прав доступу відповідно до встановленої ролі
-      </StyledHeaderTitleParagraph>
-
-      <StyledHeaderTitleParagraph>
-        &#8226; Параметризація — настройка на базі параметрів довідників кредитних продуктів, бухгалтерської моделі
-        обліку,
-        друкованих шаблонів, операцій
-      </StyledHeaderTitleParagraph>
-
-      <StyledHeaderTitleParagraph>
-        &#8226; Розрахунок резерву — «безшовна» інтеграція з модулем розрахунку резерву, автоматичний розрахунок і облік
-        резерву
-        згідно з постановою НБУ №23
-      </StyledHeaderTitleParagraph>
-
-      <StyledHeaderTitleParagraph>
-        &#8226; Інтеграція зі сторонніми системами — можливість взаємодії зі сторонніми системами зокрема з програмним
-        забезпеченням з обліку заробітної плати
-      </StyledHeaderTitleParagraph>
+          <StyledHeaderTitle>{t("Specialties.Title")}</StyledHeaderTitle>
+          <StyledHeaderTitleParagraph>
+            {t("Specialties.Line1")}
+          </StyledHeaderTitleParagraph>
+          <StyledHeaderTitleParagraph>
+            {t("Specialties.Line2")}
+          </StyledHeaderTitleParagraph>
+          <StyledHeaderTitleParagraph>
+            {t("Specialties.Line3")}
+          </StyledHeaderTitleParagraph>
+          <StyledHeaderTitleParagraph>
+            {t("Specialties.Line4")}
+          </StyledHeaderTitleParagraph>
+          <StyledHeaderTitleParagraph>
+            {t("Specialties.Line5")}
+          </StyledHeaderTitleParagraph>
 
 
-      <StyledHeaderTitle>Можливості та Переваги</StyledHeaderTitle>
-      <StyledHeaderTitleParagraph>
-        &#8226;  Автоматизація процесів виконання запитів до зовнішніх систем (БКІ, МВС і т.п.) ручних перевірок заявок
-        (дзвінок
-        на місце роботи і т.д.), перевірка бізнес правил
-      </StyledHeaderTitleParagraph>
-      <StyledHeaderTitleParagraph>
-        &#8226; Можливість розміщення пунктів по роботі з клієнтами в будь-якому місці (реалізація призначеного для
-        користувача інтерфейсу на базі Web-технологій)
-      </StyledHeaderTitleParagraph>
+          <StyledHeaderTitle>{t("Capabilities.Title")}</StyledHeaderTitle>
+          <StyledHeaderTitleParagraph>
+            {t("Capabilities.Line1")}
+          </StyledHeaderTitleParagraph>
+          <StyledHeaderTitleParagraph>
+            {t("Capabilities.Line2")}
+          </StyledHeaderTitleParagraph>
+          <StyledHeaderTitleParagraph>
+            {t("Capabilities.Line3")}
+          </StyledHeaderTitleParagraph>
+          <StyledHeaderTitleParagraph>
+            {t("Capabilities.Line4")}
+          </StyledHeaderTitleParagraph>
+          <StyledHeaderTitleParagraph>
+            {t("Capabilities.Line5")}
+          </StyledHeaderTitleParagraph>
+          <StyledHeaderTitleParagraph>
+            {t("Capabilities.Line6")}
+          </StyledHeaderTitleParagraph>
+          <StyledTestMargin/>
+        </Container>
+      </StyledBackground>
+    )
+  } 
+}
 
-      <StyledHeaderTitleParagraph>
-        &#8226;  Пряма взаємодія з пристроями захоплення зображень
-      </StyledHeaderTitleParagraph>
-
-      <StyledHeaderTitleParagraph>
-        &#8226; Адаптація робочого процесу і обробки кредитної заявки до вимог замовника
-      </StyledHeaderTitleParagraph>
-
-      <StyledHeaderTitleParagraph>
-        &#8226;  Орієнтованість на максимальну швидкість обробки кредитної заявки (видача спожитого кредиту без застави
-        за 20 хвилин)
-      </StyledHeaderTitleParagraph>
-
-      <StyledHeaderTitleParagraph>
-        &#8226; Мінімізація ризиків шахрайства шляхом автоматизації підбору параметрів і мінімізації мануального
-        введення даних
-      </StyledHeaderTitleParagraph>
-      <StyledTestMargin/>
-    </Container>
-  </StyledBackground>
-)
-
-export default Creditmodule
+export default translate("Creditmodule")(Creditmodule)
