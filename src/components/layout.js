6d898a4b4c254403fac17655598d9bc7ef9dc3b5
Dimarov <awesomedixde@gmail.com>
2019-02-22 15:41:51 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,11 +33,11 @@ const Layout = ({ children }) => (
         </Helmet>
         <script src="//instant.page/1.1.0" type="module" integrity="sha384-EwBObn5QAxP8f09iemwAJljc+sU+eUXeL9vSBw1eNmVarwhKk2F9vBEpaN9rsrtp"></script>
         <Header siteTitle={data.site.siteMetadata.title}/>
-        <form name="download" netlify hidden>
+        <form name="download" data-netlify="true" hidden>
           <input type="text" name="username" />
           <input type="text" name="email" />
         </form>
-        <form name="contact" netlify hidden>
+        <form name="contact" data-netlify="true" hidden>
           <input type="text" name="Name" />
           <input type="text" name="Email" />
           <input type="text" name="Phone" />
