0a08743e3082841fb9177bfecf76f8c2b0361070
Dimarov <awesomedixde@gmail.com>
2019-02-26 13:18:49 +0200

    i beg you work

---
+++ src/pages/success-contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SuccessContact from '../components/success-contact'
+
+const SuccessContactPage = () => (
+    <Layout>
+        <SuccessContact />
+    </Layout>
+)
+
+export default SuccessContactPage
